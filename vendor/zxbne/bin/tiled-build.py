69305385b3099c4d74a26da68f0b5ca26101083f
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2023-09-10 16:05:54 +0200

    Fixed enemies generation

--- vendor/zxbne/bin/tiled-build.py
+++ vendor/zxbne/bin/tiled-build.py
@@ -1,4 +1,5 @@
 import json
+import math
 from collections import defaultdict
 from pprint import pprint
 
@@ -64,20 +65,47 @@ with open("output/maps.bas", "w") as text_file:
 tileHeight = 16
 tileWidth = 16
 
+screenPixelsWidth = screenWidth * tileWidth
+screenPixelsHeight = screenHeight * tileHeight
+
 enemies = {}
+keys = {}
+items = {}
 
 for layer in data['layers']:
     if layer['type'] == 'objectgroup':
         for object in layer['objects']:
             if object['type'] == 'enemy':
+                xScreenPosition = math.ceil(object['x'] / screenPixelsWidth) - 1
+                yScreenPosition = math.ceil(object['y'] / screenPixelsHeight) - 1
+                screenId = xScreenPosition + (yScreenPosition * screensPerRow)
                 enemies[str(object['id'])] = {
                     'name': object['name'],
-                    'screenId': str((object['x'] // tileWidth) // screenWidth),
+                    'screenId': screenId,
                     'linIni': str(object['y'] // tileHeight % screenHeight * 16),
+                    'linEnd': str(object['y'] // tileHeight % screenHeight * 16),
                     'colIni': str(object['x'] // tileWidth % screenWidth * 2),
-                    'tile': str(object['properties'][1]['value']),
+                    'colEnd': str(object['x'] // tileWidth % screenWidth * 2),
+                    'tile': str(object['gid'] - 1),
                     'endObject': object['properties'][0]['value']
                 }
+            elif object['type'] == 'key':
+                keys[str(object['id'])] = {
+                    'name': object['name'],
+                    'screenId': screenId,
+                    'linIni': str(object['y'] // tileHeight % screenHeight * 16),
+                    'colIni': str(object['x'] // tileWidth % screenWidth * 2),
+                    'tile': str(object['gid'] - 1),
+                }
+            elif object['type'] == 'items':
+                keys[str(object['id'])] = {
+                    'name': object['name'],
+                    'screenId': screenId,
+                    'linIni': str(object['y'] // tileHeight % screenHeight * 16),
+                    'colIni': str(object['x'] // tileWidth % screenWidth * 2),
+                    'tile': str(object['gid'] - 1),
+                }
+
 for layer in data['layers']:
     if layer['type'] == 'objectgroup':
         for object in layer['objects']:
@@ -93,13 +121,69 @@ for enemyId in enemies:
         screenEnemies[enemy['screenId']] = []
     screenEnemies[enemy['screenId']].append(enemy)
 
-enemStr = "DIM enemies(" + str(len(screenEnemies)) + ",2,9) as ubyte = { _\n"
+print(screenEnemies)
 
-enemStr += '\t{ _\n\t\t{0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, _\n\t\t{0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, _\n\t\t{0, 0, 0, 0, 0, 0, 0, 0, 0, 1} _\n\t}, _\n'
+enemStr = "DIM enemies(" + str(screensCount - 1) + ",2,9) as ubyte = { _"
 
-for screenId in screenEnemies:
-    enemStr += "\t{ _\n"
-    screen = screenEnemies[screenId]
+for layer in data['layers']:
+    if layer['type'] == 'tilelayer':
+        for idx, screen in enumerate(layer['chunks']):
+            enemStr += "\n\t{ _\n"
+            if idx in screenEnemies:
+                screen = screenEnemies[idx]
+                for i in range(3):
+                    if i <= len(screen) - 1:
+                        enemy = screen[i]
+                        if (enemy['colIni'] < enemy['colEnd']):
+                            right = '1'
+                        else:
+                            right = '0'
+                        enemStr += '\t\t{' + enemy['tile'] + ', ' + enemy['linIni'] + ', ' + enemy['colIni'] + ', ' + enemy['linEnd'] + ', ' + enemy['colEnd'] + ', ' + right + ', ' + enemy['linIni'] + ', ' + enemy['colIni'] + ', 1, ' + str(i + 1) + '}, _\n'
+                    else:
+                        enemStr += '\t\t{0, 0, 0, 0, 0, 0, 0, 0, 0, ' + str(i + 1) + '}, _\n'
+            else:
+                enemStr += "\t\t{0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, _\n"
+                enemStr += "\t\t{0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, _\n"
+                enemStr += "\t\t{0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, _\n"
+            enemStr = enemStr[:-4]
+            enemStr += " _\n\t}, _"
+
+# for screenId in screenEnemies:
+#     enemStr += "\t{ _\n"
+#     screen = screenEnemies[screenId]
+#     for i in range(3):
+#         if i <= len(screen) - 1:
+#             enemy = screen[i]
+#             if (enemy['colIni'] < enemy['colEnd']):
+#                 right = '1'
+#             else:
+#                 right = '0'
+#             enemStr += '\t\t{' + enemy['tile'] + ', ' + enemy['linIni'] + ', ' + enemy['colIni'] + ', ' + enemy['linEnd'] + ', ' + enemy['colEnd'] + ', ' + right + ', ' + enemy['linIni'] + ', ' + enemy['colIni'] + ', 1, ' + str(i + 1) + '}, _\n'
+#         else:
+#             enemStr += '\t\t{0, 0, 0, 0, 0, 0, 0, 0, 0, ' + str(i + 1) + '}, _\n'
+#     enemStr = enemStr[:-4]
+#     enemStr += " _\n\t},"
+enemStr = enemStr[:-3]
+enemStr += " _\n}"
+
+with open("output/enemies.bas", "w") as text_file:
+    print(enemStr, file=text_file)
+
+screenKeys = defaultdict(dict)
+
+for keyId in keys:
+    key = keys[keyId]
+    if len(screenEnemies[key['screenId']]) == 0:
+        screenEnemies[key['screenId']] = []
+    screenEnemies[key['screenId']].append(key)
+
+keyStr = "DIM keys(" + str(len(screenEnemies)) + ",2,9) as ubyte = { _\n"
+
+keyStr += '\t{ _\n\t\t{0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, _\n\t\t{0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, _\n\t\t{0, 0, 0, 0, 0, 0, 0, 0, 0, 1} _\n\t}, _\n'
+
+for screenId in screenKeys:
+    keyStr += "\t{ _\n"
+    screen = screenKeys[screenId]
     for i in range(3):
         if i <= len(screen) - 1:
             enemy = screen[i]
@@ -107,13 +191,13 @@ for screenId in screenEnemies:
                 right = '1'
             else:
                 right = '0'
-            enemStr += '\t\t{' + enemy['tile'] + ', ' + enemy['linIni'] + ', ' + enemy['colIni'] + ', ' + enemy['linEnd'] + ', ' + enemy['colEnd'] + ', ' + right + ', ' + enemy['linIni'] + ', ' + enemy['colIni'] + ', 1, ' + str(i + 1) + '}, _\n'
+            keyStr += '\t\t{' + enemy['tile'] + ', ' + enemy['linIni'] + ', ' + enemy['colIni'] + ', ' + enemy['linEnd'] + ', ' + enemy['colEnd'] + ', ' + right + ', ' + enemy['linIni'] + ', ' + enemy['colIni'] + ', 1, ' + str(i + 1) + '}, _\n'
         else:
-            enemStr += '\t\t{0, 0, 0, 0, 0, 0, 0, 0, 0, ' + str(i + 1) + '}, _\n'
-    enemStr = enemStr[:-4]
-    enemStr += " _\n\t},"
-enemStr = enemStr[:-1]
-enemStr += " _\n}"
+            keyStr += '\t\t{0, 0, 0, 0, 0, 0, 0, 0, 0, ' + str(i + 1) + '}, _\n'
+    keyStr = keyStr[:-4]
+    keyStr += " _\n\t},"
+keyStr = keyStr[:-1]
+keyStr += " _\n}"
 
-with open("output/enemies.bas", "w") as text_file:
-    print(enemStr, file=text_file)
+with open("output/keys.bas", "w") as text_file:
+    print(keyStr, file=text_file)
